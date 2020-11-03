19cddd407260ca9f79adec3da6b8ef1caa6e4086
Alaa Fathalbab <alaa.fathalbab@gmail.com>
2020-11-03 10:53:38 +0200

    Review

--- Script.js
+++ Script.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-    $('.fa-bars').on('click', function () {
+    $('.fa-bars').on('click',function () {
         $('.nav').toggleClass('showing');
         $('.nav ul').toggleClass('showing');
     });
-
     $('.post_wrapper').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -11,33 +10,5 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
         nextArrow: $('.next'),
         prevArrow: $('.prev'),
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
     });
 });
